5f45e7f65b623f03e7c7d399392172da034014b8
Timotei Dumitru <dumitru.timy@gmail.com>
2018-08-02 23:52:49 +0100

    Menu and overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,7 @@ const menu = document.querySelector('.menu');
 const menuBtn = document.querySelector('.menu-btn');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
+const menuImg = document.querySelector('.menu-img');
 const navItems = document.querySelectorAll('.nav-item');
 
 // Set initial state of menu
@@ -17,6 +18,7 @@ function toggleMenu() {
     menuBtn.classList.add('close');
     menuNav.classList.add('show');
     menuBranding.classList.add('show');
+    menuImg.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
 
     // Set menu state
@@ -26,6 +28,7 @@ function toggleMenu() {
     menuBtn.classList.remove('close');
     menuNav.classList.remove('show');
     menuBranding.classList.remove('show');
+    menuImg.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
 
     // Set menu state
